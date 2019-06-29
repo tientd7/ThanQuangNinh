d2a78d89baf33d2f65e08d5e5f55ffc80d86dc82
Tien Tran Duy (FIS DD BOM) <tientd7@fpt.com.vn>
2019-06-29 12:23:49 +0700

    create project

---
+++ 01_Entities/Authen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public Guid ActivationCode { get; set; }
+        public virtual ICollection<Role> Roles { get; set; }
+
+    }
+
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ ThanQuangNinh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThanQuangNinh.MvcApplication" Language="C#" %>
